49636fb5f73b4944022b3b3a9bfb1fe89f113d2d
Rokonio <naelkanada+git@gmail.com>
2023-04-19 12:54:15 +0200

    Added some plugins

--- plugins.lua
+++ plugins.lua
@@ -51,15 +51,62 @@ local plugins = {
     "Pocco81/true-zen.nvim",
     cmd = { "TZAtaraxis", "TZMinimalist" },
   },
-
+  {
+    "ziontee113/color-picker.nvim",
+    cmd = "PickColor",
+    config = function()
+      require "color-picker"
+    end,
+  },
   {
 
     "lukas-reineke/indent-blankline.nvim",
     config = function()
+      vim.opt.termguicolors = true
+      -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+      vim.cmd [[highlight IndentBlanklineIndent1 guifg=#6e171e gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent2 guifg=#765517 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent3 guifg=#3b5727 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent4 guifg=#1e4c52 gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent5 guifg=#0c497a gui=nocombine]]
+      vim.cmd [[highlight IndentBlanklineIndent6 guifg=#5a1b6d gui=nocombine]]
+
       require("indent_blankline").setup {
-        -- for example, context is off by default, use this to turn it on
-        show_current_context = false,
+        show_currentdcontext = true,
         show_current_context_start = false,
+        space_char_blankline = " ",
+        char_highlight_list = {
+          "IndentBlanklineIndent1",
+          "IndentBlanklineIndent2",
+          "IndentBlanklineIndent3",
+          "IndentBlanklineIndent4",
+          "IndentBlanklineIndent5",
+          "IndentBlanklineIndent6",
+        },
+      }
+    end,
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    dependencies = {
+      "numToStr/Comment.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+      }
+      require("Comment").setup {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
       }
     end,
   },
